2d6bf995cfcd2748b7506815fa59c6f2a52cc0da
sysnetadmin2 <100679747+sysnetadmin2@users.noreply.github.com>
2023-12-18 11:26:11 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# ch 5.2.1 main.py
+import sys
+
+from ui import View
+from ctrl import Control
+from PyQt5.QtWidgets import QApplication
+
+
+def main():
+    calc = QApplication(sys.argv)
+    app = QApplication(sys.argv)
+    view = View()
+    Control(view=view)
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
